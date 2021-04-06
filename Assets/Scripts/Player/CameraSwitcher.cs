979ff33e0b6eb9611b77baf0213ab011ea08490e
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-04-07 08:22:45 +0900

    FBXExporterインポート

---
+++ Assets/Scripts/Player/CameraSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    /// <summary>カメラ切り替えキー</summary>
+    [SerializeField] string m_switchButton = "Cancel";
+    /// <summary>切り替えたい複数の Virtual Camera</summary>
+    [SerializeField] Cinemachine​Virtual​Camera​Base[] m_virtualCameras = default;
+    /// <summary>現在アクティブなカメラの Index</summary>
+    int m_cameraIndex = 0;
+
+    void Update()
+    {
+        // 設定したキーを押されたら、次のカメラに切り替える
+        if (Input.GetButtonDown(m_switchButton))
+        {
+            m_cameraIndex = (m_cameraIndex + 1) % m_virtualCameras.Length;
+            m_virtualCameras[m_cameraIndex].MoveToTopOfPrioritySubqueue();
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -1,74 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerCtrl : MonoBehaviour {
-	const float RayCastMaxDistance = 100.0f;
-	InputManager inputManager;
+public class PlayerCtrl : MonoBehaviour
+{
+    const float RayCastMaxDistance = 100.0f;
+    InputManager inputManager;
+    [SerializeField] float m_walkSpeed = 3f;
+    [SerializeField] float m_rotateSpeed = 10f;
+    CharacterController m_cc = null;
+    float m_verticalVelocity = 0f;
 
-	[SerializeField] float m_walkSpeed = 3f;
-	[SerializeField] float m_rotateSpeed = 10f;
-	CharacterController m_cc = null;
-	float m_verticalVelocity = 0f;
-	// Use this for initialization
-	void Start () 
-	{
-		inputManager = FindObjectOfType<InputManager>();
+    // Use this for initialization
+    void Start()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+        m_cc = GetComponent<CharacterController>();
+    }
 
-		Destroy(GetComponent<CharacterMove>());
-		Destroy(GetComponent<Rigidbody>());
-		Destroy(GetComponent<CapsuleCollider>());
-		m_cc = GetComponent<CharacterController>();
+    // Update is called once per frame
+    void Update()
+    {
+        Walking();
 
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		Walking();
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
 
-		float v = Input.GetAxisRaw("Vertical");
-		float h = Input.GetAxisRaw("Horizontal");
+        // 入力した方向をカメラを基準とした XZ 平面に変換する
+        Vector3 moveDirection = Vector3.forward * v + Vector3.right * h;
+        moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+        moveDirection.y = 0f;
+        moveDirection = moveDirection.normalized * m_walkSpeed;
 
-		// 入力した方向をカメラを基準とした XZ 平面に変換する
-		Vector3 moveDirection = Vector3.forward * v + Vector3.right * h;
-		moveDirection = Camera.main.transform.TransformDirection(moveDirection);
-		moveDirection.y = 0f;
-		moveDirection = moveDirection.normalized * m_walkSpeed;
+        // 入力した方向を向く
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * m_rotateSpeed);
+        }
 
-		// 入力した方向を向く
-		if (moveDirection != Vector3.zero)
-		{
-				Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-				this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * m_rotateSpeed);
-		}
-		
-		// 重力の処理
-		if (m_cc.isGrounded)
-		{
-				m_verticalVelocity = 0f;
-		}
-		else
-		{
-				m_verticalVelocity += Physics.gravity.y * Time.deltaTime;
-				moveDirection.y = m_verticalVelocity;
-		}
+        // 重力の処理
+        if (m_cc.isGrounded)
+        {
+            m_verticalVelocity = 0f;
+        }
+        else
+        {
+            m_verticalVelocity += Physics.gravity.y * Time.deltaTime;
+            moveDirection.y = m_verticalVelocity;
+        }
 
-		// 移動
-		m_cc.Move(moveDirection * Time.deltaTime);
-	}
-	
-	
-	
-	void Walking()
-	{
-		if (inputManager.Clicked()) {
-			Vector2 clickPos = inputManager.GetCursorPosition();
-			// RayCastで対象物を調べる.
-			Ray ray = Camera.main.ScreenPointToRay(clickPos);
-			RaycastHit hitInfo;
-			if(Physics.Raycast(ray,out hitInfo,RayCastMaxDistance,1 << LayerMask.NameToLayer("Ground"))) {
-				SendMessage("SetDestination",hitInfo.point);
-			}
-		}
-	}
+        // 移動
+        m_cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    void Walking()
+    {
+        if (inputManager.Clicked())
+        {
+            Vector2 clickPos = inputManager.GetCursorPosition();
+            // RayCastで対象物を調べる.
+            Ray ray = Camera.main.ScreenPointToRay(clickPos);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, RayCastMaxDistance, 1 << LayerMask.NameToLayer("Ground")))
+            {
+                SendMessage("SetDestination", hitInfo.point);
+            }
+        }
+    }
 }
