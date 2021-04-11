25ef29aba87e968752c982829de655172469795a
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-04-11 18:09:11 +0900

    再プッシュ

--- Assets/Scripts/Goblin/GoblinStatus.cs
+++ Assets/Scripts/Goblin/GoblinStatus.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoblinStatus : MonoBehaviour 
 {
@@ -12,11 +13,15 @@ public class GoblinStatus : MonoBehaviour
 
     private bool isDead = true;
 
+    //HPスライダー管理
+    public Slider goblinHPSlider;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         this.maxGoblinHp = 30;
         this.currentGoblinHp = this.maxGoblinHp;
+        this.goblinHPSlider.value = 1;
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,6 +30,7 @@ public class GoblinStatus : MonoBehaviour
         {
             int damage = 30;
             this.currentGoblinHp -= damage;
+            goblinHPSlider.value = (float)currentGoblinHp / maxGoblinHp;
         }
         if(this.currentGoblinHp <= 0)
         {
