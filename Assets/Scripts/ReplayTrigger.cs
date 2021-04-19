e4fbc2f95f57e3fdcd150d38af7a14e1d0ef9421
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-04-19 21:36:31 +0900

    足音実装中

---
+++ Assets/Scripts/ReplayTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayTrigger : MonoBehaviour
+{
+//   [SerializeField] GameObject relayTo;
+//   private FootstepSEPlayer footstepSEPlayer;
+
+//   private void Awake()
+//   {
+//     footstepSEPlayer = relayTo.GetComponent<FootstepSEPlayer>();
+//   }
+
+//   private void OnTriggerEnter(Collider other)
+//   {
+//     footstepSEPlayer.RelayedTrigger(other);
+//   }
+
+}
