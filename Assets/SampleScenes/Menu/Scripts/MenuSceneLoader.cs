c9c37e106b877f7b7b97b5387e859ae09a92ac83
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-04-22 17:11:59 +0900

    整理

--- Assets/SampleScenes/Menu/Scripts/MenuSceneLoader.cs
+++ Assets/SampleScenes/Menu/Scripts/MenuSceneLoader.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class MenuSceneLoader : MonoBehaviour
 {
-    public GameObject menuUI;
+  //   public GameObject menuUI;
 
-    private GameObject m_Go;
+  //   private GameObject m_Go;
 
-	void Awake ()
-	{
-	    if (m_Go == null)
-	    {
-	        m_Go = Instantiate(menuUI);
-	    }
-	}
+	// void Awake ()
+	// {
+	//     if (m_Go == null)
+	//     {
+	//         m_Go = Instantiate(menuUI);
+	//     }
+	// }
 }
--- Assets/SampleScenes/Menu/Scripts/SceneAndURLLoader.cs
+++ Assets/SampleScenes/Menu/Scripts/SceneAndURLLoader.cs
@@ -4,26 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneAndURLLoader : MonoBehaviour
 {
-    private PauseMenu m_PauseMenu;
+  //   private PauseMenu m_PauseMenu;
 
 
-    private void Awake ()
-    {
-        m_PauseMenu = GetComponentInChildren <PauseMenu> ();
-    }
+  //   private void Awake ()
+  //   {
+  //       m_PauseMenu = GetComponentInChildren <PauseMenu> ();
+  //   }
 
 
-    public void SceneLoad(string sceneName)
-	{
-		//PauseMenu pauseMenu = (PauseMenu)FindObjectOfType(typeof(PauseMenu));
-		m_PauseMenu.MenuOff ();
-		SceneManager.LoadScene(sceneName);
-	}
+  //   public void SceneLoad(string sceneName)
+	// {
+	// 	//PauseMenu pauseMenu = (PauseMenu)FindObjectOfType(typeof(PauseMenu));
+	// 	m_PauseMenu.MenuOff ();
+	// 	SceneManager.LoadScene(sceneName);
+	// }
 
 
-	public void LoadURL(string url)
-	{
-		Application.OpenURL(url);
-	}
+	// public void LoadURL(string url)
+	// {
+	// 	Application.OpenURL(url);
+	// }
 }
 
