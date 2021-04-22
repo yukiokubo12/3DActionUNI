9d88bbfb0a5599114e3bb1c526b56bdb268a4f20
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-04-22 17:04:17 +0900

    整理

--- Assets/SampleScenes/Scripts/PlaceTargetWithMouse.cs
+++ Assets/SampleScenes/Scripts/PlaceTargetWithMouse.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 namespace UnityStandardAssets.SceneUtils
 {
-    public class PlaceTargetWithMouse : MonoBehaviour
-    {
-        public float surfaceOffset = 1.5f;
-        public GameObject setTargetOn;
+    // public class PlaceTargetWithMouse : MonoBehaviour
+    // {
+    //     public float surfaceOffset = 1.5f;
+    //     public GameObject setTargetOn;
 
-        // Update is called once per frame
-        private void Update()
-        {
-            if (!Input.GetMouseButtonDown(0))
-            {
-                return;
-            }
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (!Physics.Raycast(ray, out hit))
-            {
-                return;
-            }
-            transform.position = hit.point + hit.normal*surfaceOffset;
-            if (setTargetOn != null)
-            {
-                setTargetOn.SendMessage("SetTarget", transform);
-            }
-        }
-    }
+    //     // Update is called once per frame
+    //     private void Update()
+    //     {
+    //         if (!Input.GetMouseButtonDown(0))
+    //         {
+    //             return;
+    //         }
+    //         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //         RaycastHit hit;
+    //         if (!Physics.Raycast(ray, out hit))
+    //         {
+    //             return;
+    //         }
+    //         transform.position = hit.point + hit.normal*surfaceOffset;
+    //         if (setTargetOn != null)
+    //         {
+    //             setTargetOn.SendMessage("SetTarget", transform);
+    //         }
+    //     }
+    // }
 }
--- Assets/SampleScenes/Scripts/SlowMoButton.cs
+++ Assets/SampleScenes/Scripts/SlowMoButton.cs
@@ -4,43 +4,43 @@ using UnityEngine.UI;
 
 namespace UnityStandardAssets.SceneUtils
 {
-    public class SlowMoButton : MonoBehaviour
-    {
-        public Sprite FullSpeedTex;     // the ui texture for full speed
-        public Sprite SlowSpeedTex;     // the ui texture for slow motion mode
-        public float fullSpeed = 1;
-        public float slowSpeed = 0.3f;
-        public Button button;           // reference to the ui texture that will be changed
+    // public class SlowMoButton : MonoBehaviour
+    // {
+    //     public Sprite FullSpeedTex;     // the ui texture for full speed
+    //     public Sprite SlowSpeedTex;     // the ui texture for slow motion mode
+    //     public float fullSpeed = 1;
+    //     public float slowSpeed = 0.3f;
+    //     public Button button;           // reference to the ui texture that will be changed
 
 
-        private bool m_SlowMo;
+    //     private bool m_SlowMo;
 
 
-       	void Start()
-        {
-			m_SlowMo = false;
-        }
+    //    	void Start()
+    //     {
+	// 		m_SlowMo = false;
+    //     }
 
-		void OnDestroy()
-		{
-			Time.timeScale = 1;
-		}
+	// 	void OnDestroy()
+	// 	{
+	// 		Time.timeScale = 1;
+	// 	}
 
-        public void ChangeSpeed()
-        {
-            // toggle slow motion state
-            m_SlowMo = !m_SlowMo;
+    //     public void ChangeSpeed()
+    //     {
+    //         // toggle slow motion state
+    //         m_SlowMo = !m_SlowMo;
 
-            // update button texture
-            var image = button.targetGraphic as Image;
-            if (image != null)
-            {
-                image.sprite = m_SlowMo ? SlowSpeedTex : FullSpeedTex;
-            }
+    //         // update button texture
+    //         var image = button.targetGraphic as Image;
+    //         if (image != null)
+    //         {
+    //             image.sprite = m_SlowMo ? SlowSpeedTex : FullSpeedTex;
+    //         }
 
-            button.targetGraphic = image;
+    //         button.targetGraphic = image;
 
-			Time.timeScale = m_SlowMo ? slowSpeed : fullSpeed;
-        }
-    }
+	// 		Time.timeScale = m_SlowMo ? slowSpeed : fullSpeed;
+    //     }
+    // }
 }
